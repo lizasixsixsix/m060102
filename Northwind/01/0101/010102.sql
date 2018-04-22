e75778f98b8a36a647931b9b6e79235fb4f5c476
lizasixsixsix <lizasixsixsix@gmail.com>
2018-04-22 23:58:59 +0300

    010102 added

---
+++ Northwind/01/0101/010102.sql
@@ -0,0 +1,7 @@
+﻿select OrderID,
+       ShippedDate =
+           case
+               when ShippedDate is null then 'Not shipped'
+           end
+    from Northwind.Orders
+    where ShippedDate is null
