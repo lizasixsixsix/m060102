730f95dbb8fd69a1dbed48ca67276ae07a67a613
lizasixsixsix <lizasixsixsix@gmail.com>
2018-04-23 00:26:42 +0300

    010103 added

---
+++ Northwind/01/0101/010103.sql
@@ -0,0 +1,9 @@
+﻿select OrderID
+           as 'Order Number',
+       case
+           when ShippedDate is null then 'Not shipped'
+		   else convert(varchar, ShippedDate, 120)
+       end
+		   as 'Shipped Date'
+    from Northwind.Orders
+    where ShippedDate >= '1998-04-07 00:00:00' or ShippedDate is null
