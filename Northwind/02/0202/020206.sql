e511730c7078c215222cb7495cab74bec62d3465
lizasixsixsix <lizasixsixsix@gmail.com>
2018-04-24 08:55:48 +0300

    020206 added

---
+++ Northwind/02/0202/020206.sql
@@ -0,0 +1,8 @@
+﻿select concat(e.LastName, ' ', e.FirstName)
+           as 'Employee',
+	   concat(m.LastName, ' ', m.FirstName)
+	       as 'Manager'
+    from Northwind.Employees e,
+	     Northwind.Employees m
+    where e.ReportsTo = m.EmployeeID
+    order by 'Manager', 'Employee'
