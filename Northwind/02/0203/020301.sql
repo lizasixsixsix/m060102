80931971b1cb9ac4aaee3618cea641728b650b7e
lizasixsixsix <lizasixsixsix@gmail.com>
2018-04-24 09:01:39 +0300

    020301 added

---
+++ Northwind/02/0203/020301.sql
@@ -0,0 +1,13 @@
+﻿select distinct Employees.EmployeeID,
+       concat(LastName, ' ', FirstName)
+	       as 'Employee'
+    from Northwind.Employees
+
+    join Northwind.EmployeeTerritories
+	    on Employees.EmployeeID = EmployeeTerritories.EmployeeID
+    join Northwind.Territories
+	    on EmployeeTerritories.TerritoryID = Territories.TerritoryID
+    join Northwind.Region
+	    on Territories.RegionID = Region.RegionID
+
+    where Region.RegionDescription = 'Western'
