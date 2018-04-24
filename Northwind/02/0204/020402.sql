8cbd21c580d68eb4ba7eda30dce9c4647519e6b2
lizasixsixsix <lizasixsixsix@gmail.com>
2018-04-24 10:11:03 +0300

    020402 added

---
+++ Northwind/02/0204/020402.sql
@@ -0,0 +1,11 @@
+﻿select concat(LastName, ' ', FirstName)
+           as 'Employee'
+    from Northwind.Employees
+    where exists
+	    (
+            select *
+			    from Northwind.Orders
+                where Employees.EmployeeID = Orders.EmployeeID
+
+				having count(*) > 150
+        )
