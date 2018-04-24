f1567c51d5803812bf0db1dfb34d5a620f740a45
lizasixsixsix <lizasixsixsix@gmail.com>
2018-04-24 08:29:19 +0300

    020202 added

---
+++ Northwind/02/0202/020202.sql
@@ -0,0 +1,12 @@
+﻿select
+    (
+	    select concat(LastName, ' ', FirstName)
+	    from Northwind.Employees
+	    where Employees.EmployeeID = Orders.EmployeeID
+	)
+	    as 'Seller',
+	count(*)
+	    as 'Amount'
+    from Northwind.Orders
+    group by EmployeeID
+    order by 'Amount' desc
