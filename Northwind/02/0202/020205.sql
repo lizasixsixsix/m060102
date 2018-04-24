58399c8807fbf7adfee60fc74efb7b3358297110
lizasixsixsix <lizasixsixsix@gmail.com>
2018-04-24 08:51:35 +0300

    020205 added

---
+++ Northwind/02/0202/020205.sql
@@ -0,0 +1,12 @@
+﻿select c.City,
+       ContactName
+    from Northwind.Customers c
+    where City in
+	    (
+            select cc.City
+			    from Northwind.Customers cc
+                group by cc.City
+
+				having count(*) > 1
+        )
+    order by c.City
