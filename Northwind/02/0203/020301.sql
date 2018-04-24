35df1f00a648a1ac8e80a190d7fed2347fc91d43
lizasixsixsix <lizasixsixsix@gmail.com>
2018-04-24 10:01:30 +0300

    020302 added

--- Northwind/02/0203/020301.sql
+++ Northwind/02/0203/020301.sql
@@ -2,12 +2,11 @@
        concat(LastName, ' ', FirstName)
 	       as 'Employee'
     from Northwind.Employees
-
-    join Northwind.EmployeeTerritories
-	    on Employees.EmployeeID = EmployeeTerritories.EmployeeID
-    join Northwind.Territories
-	    on EmployeeTerritories.TerritoryID = Territories.TerritoryID
-    join Northwind.Region
-	    on Territories.RegionID = Region.RegionID
+        join Northwind.EmployeeTerritories
+	        on Employees.EmployeeID = EmployeeTerritories.EmployeeID
+        join Northwind.Territories
+	        on EmployeeTerritories.TerritoryID = Territories.TerritoryID
+        join Northwind.Region
+	        on Territories.RegionID = Region.RegionID
 
     where Region.RegionDescription = 'Western'
---
+++ Northwind/02/0203/020302.sql
@@ -0,0 +1,10 @@
+﻿select ContactName,
+       count(*)
+	       as 'Orders Count'
+    from Northwind.Customers
+        left join Northwind.Orders
+		    on Customers.CustomerId = Orders.CustomerId
+
+    group by Customers.CustomerId,
+	         ContactName
+    order by 'Orders Count'
