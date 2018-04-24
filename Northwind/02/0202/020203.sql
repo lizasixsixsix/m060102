3825370dd26bd34b886bb54f9045181181e0d032
lizasixsixsix <lizasixsixsix@gmail.com>
2018-04-24 08:34:25 +0300

    020203 added

---
+++ Northwind/02/0202/020203.sql
@@ -0,0 +1,9 @@
+﻿select EmployeeID,
+       CustomerID,
+	   count(*)
+	       as 'Amount'
+    from Northwind.Orders
+    where year(OrderDate) = 1998
+    group by EmployeeID,
+	         CustomerID
+	order by EmployeeID
